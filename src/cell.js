6c5d5cec54302e39679fe1f2b9d713b1a41b238d
Steven Carter <stonemason@gmail.com>
2020-03-09 22:50:14 +0800

    Updated the rendering

--- src/cell.js
+++ src/cell.js
@@ -1,20 +1,23 @@
-function Cell() {
-    this.neighbours = [];
-    this.aliveNeighbours = 0;
-    this.isAlive = false;
-    this.box = null;
+import { StandardMaterial, MeshBuilder } from "@babylonjs/core";
+import { Color4 } from "@babylonjs/core"
+// import { MeshBuilder } from "@babylonjs/core";
 
-    this.create = function(scene, position) {
-        this.box = BABYLON.MeshBuilder.CreateBox('box', {size: 1, updatable: true}, scene);
+class Cell {
+    constructor(scene, position) {
+        console.log(position);
+        this.neighbours = [];
+        this.aliveNeighbours = 0;
+        this.isAlive = false;
+        this.box = MeshBuilder.CreateBox('box', { size: 1, updatable: true }, scene);
         this.box.position = position;
-        var mat = new BABYLON.StandardMaterial("material", scene);
-        mat.emissiveColor = new BABYLON.Color4(Math.random(), Math.random(), Math.random(), 1);
-        this.box.material = mat;
+        var mat = new StandardMaterial("material", scene);
+        mat.emissiveColor = new Color4(Math.random(), Math.random(), Math.random(), 1);
+        // this.box.material = mat;
         this.isAlive = Math.random() <= 0.3;
-        this.box.setEnabled(this.isAlive);
+        // this.box.setEnabled(this.isAlive);
     }
 
-    this.updateNeighbors = function() {
+    updateNeighbors() {
         this.aliveNeighbours = 0;
         for (var n in this.neighbours) {
             if (this.neighbours[n].isAlive) {
@@ -26,10 +29,12 @@ function Cell() {
                 this.isAlive = false;
             }
         } else {
-            if (this.aliveNeighbours ===3 ) {
+            if (this.aliveNeighbours === 3) {
                 this.isAlive = true;
             }
         }
         this.box.setEnabled(this.isAlive);
     }
 }
+
+export { Cell };
--- src/world.js
+++ src/world.js
@@ -1,16 +1,18 @@
+import { Cell } from './cell';
+import { Color4 } from "@babylonjs/core"
+import { Vector3 } from "@babylonjs/core/Maths/math";
 
-function World(size) {
-    this.cells = new Array();
-    this.dimension = {x: size, y: size, z: size};
-
-    this.init = function(scene) {
+class World {
+    constructor(size, scene) {
+        this.cells = new Array();
+        this.dimension = { x: size, y: size, z: size };
+        console.log(this.dimension);
         for (let x = 0; x < this.dimension.x; x++) {
             this.cells[x] = new Array();
             for (let y = 0; y < this.dimension.y; y++) {
                 this.cells[x][y] = new Array();
                 for (let z = 0; z < this.dimension.z; z++) {
-                    this.cells[x][y][z] = new Cell();
-                    this.cells[x][y][z].create(scene, new BABYLON.Vector3(x, y, z));
+                    this.cells[x][y][z] = new Cell(scene, new Vector3(x, y, z));
                 }
             }
         }
@@ -18,9 +20,9 @@ function World(size) {
         for (let x = 0; x < this.dimension.x; x++) {
             for (let y = 0; y < this.dimension.y; y++) {
                 for (let z = 0; z < this.dimension.z; z++) {
-                    for (let nx = Math.max(0, x-1); nx <= Math.min(x+1, size - 1); nx++){
-                        for (let ny = Math.max(0, y-1); ny <= Math.min(y+1, size - 1); ny++){
-                            for (let nz = Math.max(0, z-1); nz <= Math.min(z+1, size - 1); nz++){
+                    for (let nx = Math.max(0, x - 1); nx <= Math.min(x + 1, this.size - 1); nx++) {
+                        for (let ny = Math.max(0, y - 1); ny <= Math.min(y + 1, this.size - 1); ny++) {
+                            for (let nz = Math.max(0, z - 1); nz <= Math.min(z + 1, this.size - 1); nz++) {
                                 if (nx != x || ny != y || nz != z) {
                                     this.cells[x][y][z].neighbours.push(this.cells[nx][ny][nz]);
                                 }
@@ -32,15 +34,17 @@ function World(size) {
         }
     }
 
-    this.update = function(scene) {
+    update() {
         for (let x = 0; x < this.dimension.x; x++) {
             for (let y = 0; y < this.dimension.y; y++) {
                 for (let z = 0; z < this.dimension.z; z++) {
                     this.cells[x][y][z].updateNeighbors();
                     var color = this.cells[x][y][z].aliveNeighbours / this.cells[x][y][z].neighbours.length;
-                    this.cells[x][y][z].box.material.emissiveColor = new BABYLON.Color4(color, color, color, 1);
+                    // this.cells[x][y][z].box.material.emissiveColor = new Color4(color, color, color, 1);
                 }
             }
         }
     }
 }
+
+export { World };
